603d2c8834468443bafae7cce65e5302ba7e7f6f
Александр Бурый <a.buriy@it-minsk.by>
2024-01-03 11:46:02 +0300

    init

---
+++ src/test/java/by/tms/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
